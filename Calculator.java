474c1646ae440a24d231912c0f4f0d4c34e9e5f3
Wisdom <wizzy232@gmail.com>
2015-12-22 22:15:38 +0100

    Set instance variables and created main method for calculator class

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+//Calculator class
+
+//Necessary libraries
+import java.util.*; //For array list
+import javax.swing.*; //For gui components
+import java.awt.*; //For layout managers
+import java.awt.event.*; //For event handling, firing of events
+
+public class Calculator{
+
+	//Define instance variables
+	JFrame frame; //The calculator window
+	JPanel framePanel; //Content pane of the frame
+	JPanel mainPane; //Where the output and buttons will stay
+	JTextArea outPut; //Where the result will show
+	JPanel buttonPanel; //Panel for the JButtons
+	JButton clear; //Clear button;
+	String[] buttonNames = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}; //Contains the label that should show on all the number buttons
+	String[] operationNames = {"+", "-", "/", "x"};//Contains the label that should show on all the operation buttons
+
+
+	public static void main(String[] args) {
+		
+		Calculator gui = new Calculator();
+		gui.go();
+
+	}//end main method
+
+	public void go(){
+
+		
+
+	}//end go methos
+
+
+
+}//end calculator class
